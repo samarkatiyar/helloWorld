a7a2b65edde22916440dca1283d43d6c801250b2
Samar Katiyar <samar.katiyar@icloud.com>
2023-01-30 21:00:03 -0500

    Dummy Code

---
+++ dummy_or_geinus.py
@@ -0,0 +1,14 @@
+a=input("Are you a genius or a dummy -->")
+if (a=="dummy"):
+    b=input("what is 2+2 --> ")
+    if (b=="4"):
+        print (" YOU ARE NOT DUMB :)")
+    else:
+        print (" YOU ARE DUMB :(")
+else:
+    c =int(input("what is 30 * 3 -->"))
+    if (c==90):
+        print ("YOU ARE SMART :)")
+    else:
+        print ("YOU ARE NOT SMART :(")
+
