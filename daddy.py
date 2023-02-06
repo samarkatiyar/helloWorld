56210dfc09501ff9799ffc2e85e471e14a33b1cd
Samar Katiyar <samar.katiyar@icloud.com>
2023-02-06 18:58:00 -0500

    Added more stuff.

---
+++ daddy.py
@@ -0,0 +1,31 @@
+a = input("What am I thinking? --->")
+points = 0
+if a == 'What is pizza':
+    print("YOU ARE RIGHT! :)")
+    points = points + 1
+else:
+    print ("YOU ARE WRONG :(")
+    points = points - 1
+
+
+ab = input("What am I thinking? --->")
+if ab == 'Are you dumb':
+    print("YOU ARE RIGHT! :)")
+    points = points + 1
+else:
+    print ("YOU ARE WRONG :(")
+    points = points - 1
+
+
+abc = input("What am I thinking? --->")
+if abc == 'Are you strong':
+    print("YOU ARE RIGHT! :)")
+    points = points + 1
+else:
+    print ("YOU ARE WRONG :(")
+    points = points - 1
+print("your score is %s. what are you?" % points)
+
+  
+
+   
