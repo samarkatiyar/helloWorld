ddd21a59572c72ca05df2251b0ce3a951c40edfc
Samar Katiyar <samar.katiyar@icloud.com>
2023-08-10 14:05:32 -0400

    Quiz

--- quiz.py
+++ quiz.py
@@ -1,6 +1,29 @@
 import json
 import os
 import random
+import speech_recognition as sr
+import pyttsx3
+
+r = sr.Recognizer()
+def SpeakText(command):
+	engine = pyttsx3.init()
+	engine.say(command)
+	engine.runAndWait()
+	
+def hear_microphone():
+    try:
+        with sr.Microphone() as source2:
+            r.adjust_for_ambient_noise(source2, duration=0.2)
+            audio2 = r.listen(source2)
+            MyText = r.recognize_google(audio2)
+			#SpeakText(MyText)
+            return MyText 
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+        return "0"
+    except sr.UnknownValueError:
+        print("unknown error occurred")
+        return "0"
 
 def read_question_bank():
     f = open('helloWorld/data/questionbank.json')
@@ -14,17 +37,24 @@ def return_question_id(question_asked, num_questions):
 
 def get_response():
     response = ""
-    while response not in ['A', 'B', 'C', 'D']:
-          response = input("Please enter your answer ---> ")
-          response = response.upper()
-    return response
+    choices = ['one', 'two', 'three', 'four']
+    while response not in choices:
+          SpeakText("What is your answer")
+          #response = input("Please enter your answer ---> ")
+          response = hear_microphone()
+          if response:
+              response = response.lower()
+    return str((choices.index(response) + 1))
 
 def print_question(QuestionSet, question_id):
     print("-"*20)
     print("Question --> {}".format(QuestionSet[str(question_id)]['Question']))
+    SpeakText(QuestionSet[str(question_id)]['Question'])
     print("Choices")
+    SpeakText("Your choices are")
     for choice in QuestionSet[str(question_id)]['Choices']:
         print("{} : {}".format(choice, QuestionSet[str(question_id)]['Choices'][choice]))
+        SpeakText(choice + "," + QuestionSet[str(question_id)]['Choices'][choice])
 
 def check_answer(QuestionSet, question_id, response):
     if response == QuestionSet[str(question_id)]['Answer']:
@@ -38,11 +68,14 @@ def ask_question(question_asked, QuestionSet, score):
     response = get_response()
     if check_answer(QuestionSet, question_id, response):
         print("Your response is correct")
+        SpeakText("Bingo, you are correct!")
         score = score + 1
     else:
         print("Your response is not correct, the correct answer is {}".format(QuestionSet[str(question_id)]['Answer']))    
+        SpeakText("Your response is not correct, the correct answer is" + QuestionSet[str(question_id)]['Answer'])
     return question_asked, score
 
+#Start Quiz
 QuestionSet = read_question_bank()
 question_asked = [0]
 
@@ -58,3 +91,4 @@ while i < question_to_ask:
 
 score = int((score/question_to_ask)*100)
 print("Thank you for playing. Your score is {}%".format(score))
+SpeakText("Thank you for playing. Your score is " +  str(score) + "%")
